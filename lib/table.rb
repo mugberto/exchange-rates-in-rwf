eb40bc09b28ed440b7843565d253afb683ecb6c0
Hubert MUGABO <mugberto@gmail.com>
2020-08-02 23:36:40 +0200

    Add #build_wrap_cell method
    This method takes care of the content that
    overflows the cell.

--- lib/table.rb
+++ lib/table.rb
@@ -9,6 +9,20 @@ class Table
   def draw(path)
     @data = CSV.read(path)
     col_widths
+    table = ''
+    @data.each do |row|
+      line = ''
+      row.each_with_index { |content, index| line += build_cell(@col_widths[index], content, index) }
+      i = 1
+      until @breaklines.zero?
+        row.each_with_index { |content, index| line += build_wrap_cell(@col_widths[index], content, index, i) }
+        i += 1
+      end
+      line << "\n" + '-' * (@col_widths.sum + @col_widths.size + 1)
+      @breakcells = []
+      table << line
+    end
+    table << "\n"
   end
 
   def col_widths
@@ -28,6 +42,19 @@ class Table
     else
       @breaklines = data.size / cell_size
       (index.zero? ? "\n|" : '') + data[0, cell_size - 1] + '-|'
+    end
+  end
+
+  def build_wrap_cell(cell_size, data, index, i)
+    if data.size <= cell_size
+      (index.zero? ? "\n|" : '') + ' ' * cell_size + '|'
+    elsif @breaklines == 1
+      @breaklines = 0
+      n = cell_size * (data.size / cell_size + 1)
+      (index.zero? ? "\n|" : '') + data[(n - cell_size - i)..-1] + ' ' * (n - data.size - i) + '|'
+    else
+      @breaklines -= 1
+      (index.zero? ? "\n|" : '') + data[(i * cell_size - 1), (cell_size - 1)] + '-|'
     end
   end
 end
