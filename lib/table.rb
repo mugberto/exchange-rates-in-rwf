2efb8a79451f992b8eb81c68a1f15ca00e0cd888
Hubert MUGABO <mugberto@gmail.com>
2020-08-02 23:15:39 +0200

    Add #col_widths method
    This method populates @col_widths array with values
    that represent the widths of the table columns

--- lib/table.rb
+++ lib/table.rb
@@ -1,5 +1,24 @@
+require 'csv'
+
 class Table
   def initialize
     @data = []
+    @col_widths = []
+  end
+
+  def draw(path)
+    @data = CSV.read(path)
+    col_widths
+  end
+
+  def col_widths
+    cols = @data.max { |a, b| a.length <=> b.length }
+    cols.size.times do |i|
+      arr = []
+      @data.each do |row|
+        arr << row[i].length
+      end
+      @col_widths[i] = arr.max <= 15 ? arr.max : @max_col_width
+    end
   end
 end
