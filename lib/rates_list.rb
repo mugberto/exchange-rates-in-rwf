da01deb7661c35cf2d5197cb04c9c8b032af426d
Hubert MUGABO <mugberto@gmail.com>
2020-08-05 17:49:33 +0200

    Comment out lines that depends on the Selenium gem

--- lib/rates_list.rb
+++ lib/rates_list.rb
@@ -1,5 +1,5 @@
 require 'nokogiri'
-require 'selenium-webdriver'
+# require 'selenium-webdriver'
 require 'csv'
 require 'open-uri'
 require_relative './urls.rb'
@@ -27,19 +27,24 @@ class RatesList
   private
 
   def extract_rates(url)
-    driver = Selenium::WebDriver.for :chrome
-    driver.navigate.to url
+    # driver = Selenium::WebDriver.for :chrome
+    # driver.navigate.to url
     begin
-      expand_element = driver.find_element(id: 'recordLimit')
-      driver.action.click(expand_element).key_down(:arrow_down).key_down(:arrow_down).key_down(:enter).perform
-      sleep 3
-      rows = Nokogiri::HTML(driver.page_source).css('tbody tr')
-    rescue Selenium::WebDriver::Error::WebDriverError
-      return 'error'
+      # expand_element = driver.find_element(id: 'recordLimit')
+      # driver.action.click(expand_element).key_down(:arrow_down).key_down(:arrow_down).key_down(:enter).perform
+      # sleep 3
+      # rows = Nokogiri::HTML(driver.page_source).css('tbody tr')
+      # driver.quit
+      # rubocop:disable Security/Open
+      rows = Nokogiri::HTML(open(url)).css('tbody tr')
+    # rubocop:enable Security/Open
+    # rescue Selenium::WebDriver::Error::WebDriverError
+    #   return 'error'
     rescue Nokogiri::CSS::Tokenizer::ScanError
       return 'error'
+    rescue OpenURI::HTTPError
+      return 'error'
     end
-    driver.quit
     rows.each do |row|
       row = row.css('td').map { |i| i.text.strip }
       row.shift
@@ -48,15 +53,24 @@ class RatesList
   end
 
   def add_country_name(url)
-    driver = Selenium::WebDriver.for :chrome
+    # driver = Selenium::WebDriver.for :chrome
+    # begin
+    #   driver.navigate.to url
+    #   sleep 3
+    #   country_list = CSV.parse(Nokogiri::HTML(driver.page_source).css('pre').text)
+    #   driver.quit
+    # rescue Selenium::WebDriver::Error::WebDriverError
+    #   return 'error'
+    # rescue Nokogiri::CSS::Tokenizer::ScanError
+    #   return 'error'
+    # end
     begin
-      driver.navigate.to url
-      sleep 3
-      country_list = CSV.parse(Nokogiri::HTML(driver.page_source).css('pre').text)
-      driver.quit
-    rescue Selenium::WebDriver::Error::WebDriverError
-      return 'error'
+      # rubocop:disable Security/Open
+      country_list = CSV.parse(Nokogiri::HTML(open(url)).css('p').children[0].text)
+      # rubocop:enable Security/Open
     rescue Nokogiri::CSS::Tokenizer::ScanError
+      return 'error'
+    rescue OpenURI::HTTPError
       return 'error'
     end
     @data.each do |row|
